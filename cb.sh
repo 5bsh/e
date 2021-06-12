8dcffbe30fd35533053ee3ef122fa1f7afc275d0
5bsh <47255826+5bsh@users.noreply.github.com>
2021-06-13 03:23:06 +0530

    updated

--- cb.sh
+++ cb.sh
@@ -5,6 +5,9 @@
 #backup old resolve.conf file
 mv /etc/resolv.conf /etc/resolve.conf.bak
 
+#create a new resolv.conf file
+touch /etc/resolve.conf
+
 # add bew resolv.conf file
 echo 'nameserver 185.228.168.168' > /etc/resolv.conf
 
