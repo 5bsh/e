5de14c142063fe9c898db6d485073d0c13369835
5bsh <47255826+5bsh@users.noreply.github.com>
2021-06-13 03:16:55 +0530

    updated with comments

--- cb.sh
+++ cb.sh
@@ -1,2 +1,15 @@
-sudo echo 'nameserver 185.228.168.168' > /etc/resolv.conf
-sudo chattr +i /etc/resolv.conf
+#!/bin/bash
+
+# use superuser to run these commands
+
+#backup old resolve.conf file
+mv /etc/resolv.conf resolve.conf.bak
+
+# add bew resolv.conf file
+echo 'nameserver 185.228.168.168' > /etc/resolv.conf
+
+#Add attribution to avoid changes to file
+chattr +i /etc/resolv.conf
+
+#restart network-manager service
+service network-manager restart
